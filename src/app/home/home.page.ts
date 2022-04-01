2604e5292b37174712926a1764df9946c2a01252
alegallardo@us.es <alegallardo@us.es>
2022-04-01 12:01:30 +0200

    new

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,9 @@
 import { Component, NgZone } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
 
+
 import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
-import {AlertController, ToastController } from '@ionic/angular';
+import {AlertController, IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,27 +12,116 @@ import {AlertController, ToastController } from '@ionic/angular';
 })
 export class HomePage {
 devices: any[] = [];
-
+unpairedDevices: any;
+pairedDevices: any;
+gettingDevices: boolean;
+connected: boolean = false;
 
   constructor(private ble: BLE,
               private ngZone: NgZone,
-              private bluetoothSerial: BluetoothSerial) {}
+              private bluetoothSerial: BluetoothSerial,
+              private alertController: AlertController) {
+                bluetoothSerial.enable();
+              }
 
-Scan(){
-  console.log("he llegado aquí");
-  this.devices = [];
-  this.bluetoothSerial.list().then(
-    device => this.onDeviceDiscovered(device)
-  );
-  
-}
-
-onDeviceDiscovered(device){
-  console.log('Discovered' + JSON.stringify(device,null,2));
-  this.ngZone.run(()=>{
-    this.devices.push(device)
-    console.log(device)
-  })
-}
-
-}
+              
+              startScanning() {
+                this.pairedDevices = null;
+                this.unpairedDevices = null;
+                this.gettingDevices = true;
+                const unPair = [];
+                this.bluetoothSerial.discoverUnpaired().then((success) => {
+                  success.forEach((value, key) => {
+                    var exists = false;
+                    //console.log("esto es success", value);
+                    unPair.forEach((val2, i) => {
+                      //console.log("esto es unPair", val2);
+                      if (value.id === val2.id) {
+                        exists = true;
+                      }
+                    });
+                    if (exists === false && value.id !== '') {
+                      unPair.push(value);
+                    }
+                  });
+                  this.unpairedDevices = unPair;
+                  this.gettingDevices = false;
+                },
+                  (err) => {
+                    console.log(err);
+                  });
+              
+                this.bluetoothSerial.list().then((success) => {
+                  this.pairedDevices = success;
+                },
+                  (err) => {
+              
+                  });
+                }
+              
+              success = (data) => {
+                this.deviceConnected();
+              }
+              fail = (error) => {
+                alert(error);
+              }
+              
+              async selectDevice(id: any) {
+              
+                const alert = await this.alertController.create({
+                  header: 'Connect',
+                  message: 'Do you want to connect with?',
+                  buttons: [
+                    {
+                      text: 'Cancel',
+                      role: 'cancel',
+                      handler: () => {
+                        console.log('Cancel clicked');
+                      }
+                    },
+                    {
+                      text: 'Connect',
+                      handler: () => {
+                        this.bluetoothSerial.connect(id).subscribe(this.success, this.fail);
+                      }
+                    }
+                  ]
+                });
+                await alert.present();
+              }
+              
+              deviceConnected() {
+                
+                this.bluetoothSerial.isConnected().then(success => {
+                  alert('Connected Successfullly');
+                  this.connected = true;
+                  document.getElementById("devices").innerHTML = "";
+                }, error => {
+                  alert('error' + JSON.stringify(error));
+                });
+              }
+              
+              async disconnect() {
+                const alert = await this.alertController.create({
+                  header: 'Disconnect?',
+                  message: 'Do you want to Disconnect?',
+                  buttons: [
+                    {
+                      text: 'Cancel',
+                      role: 'cancel',
+                      handler: () => {
+                        console.log('Cancel clicked');
+                      }
+                    },
+                    {
+                      text: 'Disconnect',
+                      handler: () => {
+                        this.bluetoothSerial.disconnect();
+                        this.connected = false;
+                      }
+                    }
+                  ]
+                });
+                await alert.present();
+              }
+             }
